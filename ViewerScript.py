9f8a826c4437fa18bd7940a9f64847e45ece80c9
Bluesbreaker45 <bluesbreaker45@outlook.com>
2025-08-04 10:42:10 +0800

    ViewerScript supports videos.

--- ViewerScript.py
+++ ViewerScript.py
@@ -18,6 +18,9 @@ header = """\
     .content img {
         width: 800px;
     }
+    .content video {
+        width: 800px;
+    }
 </style>
 
 <body>
@@ -35,19 +38,43 @@ content =  """\
     </div>
 """
 
+video_content = """\
+    <div class="content">
+        <video src="{}" controls>
+            <p>Your browser doesn't support HTML5 video. Here is a <a href="{}">link to the video</a> instead.</p>
+        </video>
+    </div>
+"""
+
 def gen_html(url, outputName):
     if any(("viewer.html" in f.name.lower()) for f in os.scandir(url)):
         return
-    if not any(f.name.lower().endswith((".jpg", ".png", ".jpeg", ".webp")) for f in os.scandir(url)):
+    
+    # 检查是否有图片或视频文件
+    has_images = any(f.name.lower().endswith((".jpg", ".png", ".jpeg", ".webp")) for f in os.scandir(url))
+    has_videos = any(f.name.lower().endswith((".mp4", ".avi", ".mkv", ".mov", ".wmv", ".flv", ".webm")) for f in os.scandir(url))
+    
+    if not has_images and not has_videos:
         return
-    files = filter(lambda x: x.lower().endswith((".jpg", ".png", ".jpeg", ".webp")), os.listdir(url))
+    
+    # 获取视频文件
+    video_files = []
+    if has_videos:
+        video_files = list(filter(lambda x: x.lower().endswith((".mp4", ".avi", ".mkv", ".mov", ".wmv", ".flv", ".webm")), os.listdir(url)))
+    
+    # 获取图片文件
+    image_files = []
+    if has_images:
+        image_files = list(filter(lambda x: x.lower().endswith((".jpg", ".png", ".jpeg", ".webp")), os.listdir(url)))
 
     def extract_name(name):
         f = filter(lambda x: True if x else False, re.split("([0-9]+)|([^0-9]+)", name))
         m = map(lambda x: int(x) if x.isdigit() else x, f)
         return list(m)
     
-    files = sorted(files, key=extract_name)
+    # 分别排序视频和图片文件
+    video_files = sorted(video_files)
+    image_files = sorted(image_files, key=extract_name)
 
     if url[-1] != '/':
         url += '/'
@@ -55,16 +82,28 @@ def gen_html(url, outputName):
     debugCount = 0
     printNum = 5
     contentStr = ""
-    for file in files:
+    
+    # 首先添加视频文件
+    for file in video_files:
         if debugCount < printNum:
-            print(file)
+            print(f"Video: {file}")
+        elif debugCount == printNum:
+            print("...", end=" ")
+        debugCount += 1
+        contentStr += video_content.format(file, url + file)
+    
+    # 然后添加图片文件
+    for file in image_files:
+        if debugCount < printNum:
+            print(f"Image: {file}")
         elif debugCount == printNum:
             print("...", end=" ")
         debugCount += 1
         contentStr += content.format(file, url + file)
-        #contentStr += "<img src=\"" + file + "\" alt=\"" + file + "\" width=800px>\n"
-    if len(files) >= printNum:
-        print(len(files) - printNum, "more pics.")
+    
+    total_files = len(video_files) + len(image_files)
+    if total_files >= printNum:
+        print(f"{total_files - printNum} more files.")
 
     html = open(url + outputName, "w", encoding="UTF-8")
     html.write(header + contentStr + tail)
@@ -84,7 +123,7 @@ def walkDir(dir, newName):
 def main():
     parser = argparse.ArgumentParser(description="Generate HTML for pictures.")
     parser.add_argument("-d", dest="dir", metavar="src_directory", default=".", help="path to source pictures' directory (default: current working directory \".\")")
-    parser.add_argument("-o", dest="newName", metavar="output's_name", default="Viewer.html", help="the name of the output pdf (default: \"viewer.html\")")
+    parser.add_argument("-o", dest="newName", metavar="output's_name", default="Viewer.html", help="the name of the output html file (default: \"Viewer.html\")")
 
     args = parser.parse_args()
     walkDir(args.dir, args.newName)
